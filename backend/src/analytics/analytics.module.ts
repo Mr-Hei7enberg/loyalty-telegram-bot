7fc3c84661800222933a40de14d8eebd28ab33ac
Mr-Hei7enberg <63899989+Mr-Hei7enberg@users.noreply.github.com>
2025-09-25 11:56:06 +0300

    Import AuthModule into analytics module

--- backend/src/analytics/analytics.module.ts
+++ backend/src/analytics/analytics.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from '../auth/auth.module';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 import { UserActionLog } from './entities/user-action-log.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([UserActionLog])],
+  imports: [SequelizeModule.forFeature([UserActionLog]), AuthModule],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService, SequelizeModule],
